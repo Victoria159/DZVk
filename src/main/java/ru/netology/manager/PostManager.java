d2323773913b4e720265ade2c8e68cdf3c51d816
Victoria159 <v.s.bocha@gmail.com>
2021-08-19 22:02:25 +0300

    Changed PostManager.java

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -3,47 +3,20 @@ package ru.netology.manager;
 import ru.netology.domain.Post;
 
 public class PostManager {
-private Post[] posts;
-//
-//    public Post[]search(){
-////        TODO: add logic
-//        return null;
-//    }
-//    public void delete(){
-//        // TODO: add logic
-//    }
-    private int ownerId;  // идентификатор пользователя или сообщества
-    private String address; // адрес пользователя или сообщества
-    private String query; // поисковый запрос
-    private boolean ownersOnlyId;  // записи только данного пользователя
-    private int count;  // количество записей, соответствующих запросу
-    private int postId;  // идентификатор записи
+    private Post[] posts;
+    private int ownerId;
+    private String address;
+    private String query;
+    private boolean ownersOnlyId;
+    private int count;
+    private int postId;
 
-    public Post[] searchOwnerId(){
-//        TODO: add logic
+
+    public Post[] WallSearch(int idOwner, String address, String query, boolean idOwnersOnly, int count) {
+//        add logic
         return null;
     }
-    public Post[] searchAddress() {
-//        TODO: add logic
-        return null;
-    }
-    public Post[] searchQuery() {
-//        TODO: add logic
-        return null;
-    }
-    public Post[] searchOwnersOnlyId() {
-//        TODO: add logic
-        return null;
-    }
-    public Post[] searchCount() {
-//        TODO: add logic
-        return null;
-    }
-    public Post[] searchPostId() {
-//        TODO: add logic
-        return null;
-    }
-    public void deleteOwnersOnlyId() {
-//        TODO: add logic
+    public void WallDelete(int idOwner, int idPost){
+
     }
 }
